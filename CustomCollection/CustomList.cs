168a531796292fb0360d3af29e3e0e58d93f0ef4
Karthik Patlolla <Karthik_Patlolla@epam.com>
2020-01-31 14:42:38 +0530

    Updated

--- CustomCollection/CustomList.cs
+++ CustomCollection/CustomList.cs
@@ -7,39 +7,69 @@ using System.Threading.Tasks;
 
 namespace CustomCollection
 {
-    class CustomList : IList
+   public class CustomList : IList,IEnumerable
     {
+
+        // Implementation for the GetEnumerator method.
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public ArrayListEnum GetEnumerator()
+        {
+            return new ArrayListEnum(this);
+        }
+        //Global array of objects initialization
         object[] array;
         int count = 0;
-        object[] tmp;
+        //Constructor 
         public CustomList()
         {
             array = new object[0];
            // Console.WriteLine(array.Length);
         }
+        //Constructor with size parameter.
         public CustomList(int size)
         {
-
+            array = new object[size];
         }
+        
         public object this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public bool IsReadOnly => throw new NotImplementedException();
+        /// <summary>
+        /// This method returns Capacity of CustomList
+        /// </summary>
+        public int Capacity
+        {
+            get => array.Length;
+        }
 
         public bool IsFixedSize => throw new NotImplementedException();
-
-        public int Count => throw new NotImplementedException();
+        /// <summary>
+        /// This method returns Count of CustomList
+        /// </summary>
+        public int Count
+        {
+            get => this.count;
+        }
 
         public object SyncRoot => throw new NotImplementedException();
 
         public bool IsSynchronized => throw new NotImplementedException();
-
+        /// <summary>
+        /// This method adds an element into CustomList
+        /// </summary>
         public int Add(object value)
         {
-            if (count == 0)
+            object[] tmp;
+
+            if (this.count == 0)
             {
                 array = new object[4];
             }
-            if (count == array.Length)
+            if (this.count == array.Length)//increasing size of Customlist
             {
                 tmp = new object[array.Length + array.Length];
 
@@ -49,18 +79,18 @@ namespace CustomCollection
                 }
                 array = tmp;
             }
-            //tmp[tmp.Length - 1] = value;
-            //Console.WriteLine("Hello "+array.Length);
-            array[count] = value;
-            count++;
-            return count-1;
+            array[this.count] = value;
+            this.count++;
+            return this.count-1;
         }
 
         public void Clear()
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// This method returns boolean value(true) if given value is present in CustomList
+        /// </summary>
         public bool Contains(object value)
         {
             return IndexOf(value) != -1;
@@ -71,29 +101,209 @@ namespace CustomCollection
             throw new NotImplementedException();
         }
 
-        public IEnumerator GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// This method returns the index of given element in CustomList
+        /// </summary>
         public int IndexOf(object value)
         {
             return Array.IndexOf(array, value);
         }
-
+        /// <summary>
+        /// This method inserts an element into the CustomList at specified Index.
+        /// </summary>
         public void Insert(int index, object value)
         {
-            int k = 0;
-                 }
+            object[] tmp;
 
+            if (index < 0 && index > array.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+      
+                if (this.count == array.Length)
+                {
+                    tmp = new object[2 * array.Length];
+                }
+                else
+                {
+                    tmp = new object[array.Length];
+                }
+                for(int i = 0; i < index; i++)
+                {
+                    tmp[i] = array[i];
+                }
+      
+                tmp[index] = value;
+                for (int i = index; i < count; i++)
+                {
+                    tmp[i + 1] = array[i];
+                }
+                array = tmp;
+                tmp = null;
+                this.count++;
+            }
+        }
+        /// <summary>
+        /// This method removes given element from the CustomList
+        /// </summary>
         public void Remove(object value)
         {
-            throw new NotImplementedException();
+            object[] tmp;
+            int flag = 0;
+            tmp = new object[array.Length];
+            for (int i = 0, j = 0; i < array.Length && j < tmp.Length; i++,j++)
+            {
+                //Console.WriteLine("!"+array[i]);
+                if (value.Equals(array[i])&&flag==0)
+                {
+                    //Console.WriteLine("Removal");
+                    i++;
+                    flag = 1;
+                }
+                tmp[j] = array[i]; 
+            }
+            array = tmp;
+            if (flag == 1) 
+            {
+                this.count--;
+                //Console.WriteLine("done");
+            }
         }
-
+        /// <summary>
+        /// This method removes an element at specified index from CustomList.
+        /// </summary>
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            object[] tmp;
+            if (index < 0 && index > array.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                int flag = 0;
+                tmp = new object[array.Length];
+                for (int i = 0, j = 0; i < array.Length && j < tmp.Length; i++, j++)
+                {
+                    if (i == index && flag == 0)
+                    {
+                        i++;
+                        flag++;
+                    }
+                    tmp[j] = array[i];
+                }
+                array = tmp;
+                if (flag>0)
+                {
+                    this.count -= flag;
+                }
+            }
         }
+        /// <summary>
+        /// This method searches for a specific element in CustomList.
+        /// </summary>
+        public int find(object value)
+        {
+            foreach(object i in array)
+            {
+                if (value.Equals(i))
+                {
+                    return IndexOf(i);
+                }
+                
+            }
+            return -1;
+        }
+        /// <summary>
+        /// This method returns element at a specific index in CustomList. 
+        /// </summary>
+        public Object Get(int index)
+        {
+            if (index < 0 && index > array.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                return array[index];
+            }
+        }
+        /// <summary>
+        /// This method sorts the CustomList.
+        /// </summary>
+        public void Sort()
+        {
+            Array.Sort(array,0,count);
+        }
+        public void Reverse()
+        {
+            //object[] tmp = new object[array.Length];
+            //Console.WriteLine(count);
+            //Console.WriteLine("In reverse:");
+            //foreach(object i in array)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 0, j = count; i > count; i++, j--)
+            //{
+            //    Console.WriteLine("hello");
+            //    tmp[i] = array[j - 1];
+
+            //}
+            //array = tmp;   
+            Array.Reverse(array, 0, count);
+        }
+     
+    }
+    // When you implement IEnumerable, you must also implement IEnumerator.
+    public class ArrayListEnum : IEnumerator
+    {
+        public CustomList _list;
+
+        // Enumerators are positioned before the first element
+        // until the first MoveNext() call.
+        int position = -1;
+
+        public ArrayListEnum(CustomList list)
+        {
+            _list = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < _list.Count);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public Object Current
+        {
+            get
+            {
+                try
+                {
+                    return _list.Get(position);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+        
     }
 }
---
+++ CustomCollection/CustomListDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCollection
+{
+    class CustomListDemo
+    {
+        static void Main(string[] args)
+        {
+            CustomList cl = new CustomList();
+            cl.Add(10);
+            cl.Add(20);
+            cl.Add(50);
+            cl.Add(60);
+            cl.Add(60);
+            cl.Add(60);
+            cl.Add(60);
+            cl.Add(10);
+            cl.Add(20);
+            cl.Add(50);
+            cl.Add(60);
+            cl.Add(60);
+            cl.Add(60);
+            cl.Add(60);
+            cl.Add(100);
+            cl.Insert(2, 100);
+            cl.Remove(10);
+            cl.RemoveAt(1);
+            Console.WriteLine("Contains:" + cl.Contains(60));
+            Console.WriteLine("Index:" + cl.find(50));
+            Console.WriteLine(cl.Get(1));
+            Console.WriteLine("Count:" + cl.Count);
+            Console.WriteLine("Capacity:" + cl.Capacity);
+            cl.Reverse();
+            Console.WriteLine("After Reversing:");
+            foreach (object i in cl)
+            {
+                Console.WriteLine(i);
+            }
+            cl.Sort();
+            Console.WriteLine("AfterSorting");
+            foreach (object i in cl)
+            {
+                Console.WriteLine("Sorted:" + i);
+            }
+
+            Console.ReadLine();
+            
+        }     
+    }
+}
